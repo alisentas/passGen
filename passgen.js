f175d2163840c849129ce6ed05600f0c5a21cc0b
Ali Sentas <alisentas96@gmail.com>
2015-08-26 21:23:12 +0300

    added support for default parameters

--- passgen.js
+++ passgen.js
@@ -4,37 +4,38 @@ var passGen = function(opts){
 	/**
 	 * opts object's structure:
 		opts = {
-	    	length: int,
-			readable: false,
-			letter: true,
-			number: true,
-			upperCase: true,
-			specialChar: false
+	    	length: 	 int  (required),
+			readable: 	 bool (default=false),
+			letter: 	 bool (default=true),
+			number: 	 bool (default=true),
+			upperCase: 	 bool (default=true),
+			specialChar: bool (default=true)
 		}
 		*/
-	//Decleare every required variables
+	// Decleare every required variables
 	var password = "",
 	passwordSplit,
 	length = opts.length,
-	readable = opts.readable,
-	letter = opts.letter,
-	number = opts.number,
-	upperCase = opts.upperCase,
-	specialChar = opts.specialChar,
+	readable = opts.readable || false,
+	letter = opts.letter == undefined ? true : opts.letter,
+	number = opts.number == undefined ? true : opts.number,
+	upperCase = opts.upperCase == undefined ? true : opts.upperCase,
+	specialChar = opts.specialChar == undefined ? true : opts.specialChar,
 	letters = {
-		//letter object, contains both consonants and vowels
-		//consonants and vowels are seperated because of the readability option
-		consonants: ["b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","y","z","x","q","w"],
-		vowels: ["a","e","i","o","u"]
+		// letter object, contains both consonants and vowels
+		// consonants and vowels are seperated because of the readability option
+		consonants: "bcdfghjklmnpqrstvwxyz",
+		vowels: "aeiou",
 	},
 	numbers = [0,1,2,3,4,5,6,7,8,9],
-	specialChars = ["!","+","%","&","(",")","=","?","_","-","*","-","#","$","½","{","[","]","}",",",".","<",">"],
-	prevVowel = false, prevConso = false, prevConsox2 = false,//these variables are for readability part
+	specialChars = ["!","+","%","&","(",")","=","?","_","-","*","-","#","$","ï¿½","{","[","]","}",",",".","<",">"],
+	prevVowel = false, prevConso = false, prevConsox2 = false,// these variables are for readability part
 	i, chars;
+
 	if(readable){
-		//What we do here is this, if the password ends with a vowel, we can add a consonant, 
-		//if the password ends with a consonant, we can add a vowel or a consonant,
-		//if the password ends with double consonant, we should add a vowel
+		// What we do here is this, if the password ends with a vowel, we can add a consonant,
+		// if the password ends with a consonant, we can add a vowel or a consonant,
+		// if the password ends with double consonant, we should add a vowel
 		if(Math.floor(Math.random() * 2)){
 			prevVowel = true;
 		}else{
@@ -75,22 +76,23 @@ var passGen = function(opts){
 			password = passwordSplit.join("");
 		}
 	}else{
-		chars = [];//character holder array for our passwords
-		if(letter) chars = chars.concat(letters.consonants, letters.vowels);
+		chars = []; // character holder array for our passwords
+		if(letter) chars = chars.concat(letters.consonants.split(""), letters.vowels.split(""));
 		if(number) chars = chars.concat(numbers);
 		if(specialChar) chars = chars.concat(specialChars);
 		for (i = 0; i < length; i++) {
 			password += chars[Math.floor(Math.random() * chars.length)];
 		}
-	}
-	if(upperCase){
-		passwordSplit = password.split("");
-		for(i in passwordSplit){
-			if(Math.floor(Math.random() * 2)){
-				passwordSplit[i] = passwordSplit[i].toUpperCase();
+		if(upperCase){
+			passwordSplit = password.split("");
+			for(i in passwordSplit){
+				if(Math.floor(Math.random() * 2)){
+					passwordSplit[i] = passwordSplit[i].toUpperCase();
+				}
 			}
+			password = passwordSplit.join("");
 		}
-		password = passwordSplit.join("");
 	}
+
 	return password;
 };
--- readme.md
+++ readme.md
@@ -1,9 +1,7 @@
-
-
 passGen - Password Generator
 ====
 
-![Download](http://img.shields.io/badge/download-2.953%20kb-blue.svg)  |  ![Version](http://img.shields.io/badge/version-1.0.0-brightgreen.svg) 
+![Download](http://img.shields.io/badge/download-2.953%20kb-blue.svg)  ![Version](http://img.shields.io/badge/version-1.0.0-brightgreen.svg)
 
 Passgen is a powerful password generator. You can also use it for generating random strings.
 
